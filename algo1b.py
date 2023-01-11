19828fdeae769773d01570ff6e119850b8003e86
Ayush Singh Rathour <ayushsinghrathour@Ayushs-MBP.lan>
2023-01-11 05:30:13 -0500

    1st question 2 view

--- algo1b.py
+++ algo1b.py
@@ -1,13 +1,9 @@
 def twoNumberSum(array, targetSum):
-    array.sort()
-    left = 0
-    right = len(array) - 1
-    while left < right:
-        currentsum = array[left] + array[right]
-        if currentsum == targetSum:
-            return [array[left], array[right]]
-        elif currentsum < targetSum:
-            left += 1
-        elif currentsum > targetSum:
-            right -= 1
+    hashtable = {}
+    for i in array:
+        y = targetSum - i
+        if y in hashtable:
+            return[y, i]
+        else:
+            hashtable[i] = True
     return []
